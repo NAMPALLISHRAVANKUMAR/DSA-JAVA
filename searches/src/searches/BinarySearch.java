4dc4043b2973b9b24cf2204798d104a50bbd26e0
SHRAVAN KUMAR NAMPALLI <shravankumarnampalli@gmail.com>
2022-05-29 12:03:40 +0530

    searching algo added , binary search

---
+++ searches/src/searches/BinarySearch.java
@@ -0,0 +1,34 @@
+package searches;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4,5,6,7,8,9,10,11,15,16,19};
+		int key=190;
+		int index=binarySearch(arr,key);
+		if(index==-1) 
+			System.out.println("Sorry the number you are trying to search does not exists!!");
+		else
+			System.out.println("The number found at index "+ index);
+
+	}
+  static int binarySearch(int arr[],int key) {
+	  int l=0;
+	  int r=arr.length -1;
+	 
+	  
+	  while(l<=r) {
+		  int mid=l+(r-l)/2;
+		  System.out.println("The mid value is "+ mid);
+		  if(arr[mid]==key) 
+			  return mid; //if the given key is at mid index
+		  else if(arr[mid]<key) 
+			  l=mid+1; // if the given key is greater than the mid value then left index shoud be moved to mid+1
+		  else 
+			  r=mid-1; // if the given key is less than the mid value then right index shoud be moved to mid-1
+	  }
+	  return -1; //if while loop exits then it means we did not found the key,so we are sending a negative number since array index cant be negative.
+  }
+}
+
+//the worst case time complexity will be O(log n) and the best case will be O(1) 
